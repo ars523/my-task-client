6f3aad68ece4e3056f29562750b8155e3fc62720
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2022-08-08 07:52:31 +0600

    status design updated

--- src/componests/Tasks.jsx
+++ src/componests/Tasks.jsx
@@ -1,7 +1,7 @@
 import { Grid, Paper, Stack, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
-import {useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { HeadingPrimary } from '../shared/heading';
 import Spinner from './Spinner';
 import EditModal from './EditModal';
@@ -26,35 +26,39 @@ const Tasks = (props) => {
                         key={task._id}
                     >
                         <Paper sx={{
-                            p: '1rem', 
+                            p: '1rem',
                             height: '100%',
                             display: 'flex',
                             flexDirection: 'column',
                             justifyContent: 'space-between'
                         }}
                         >
-                                <Typography variant='h5' component='h2' sx={{mb: '0.75rem', color: 'grey.800'}}>{task.todo}</Typography>
-                                <Typography variant='subtitle1' sx={{mb: '0.75rem', color: 'grey.700'}}>{task.description}</Typography>
-                                <Stack direction='raw' justifyContent='space-between' alignItems='center'>
-                                    <Stack direction='raw' alignItems='center'>
-                                        <Typography>{new Date(task.createdAt).toLocaleString('en-us')}</Typography>
-                                        <EditModal taskId={task._id} status={task.status} />
-                                        <DeleteDialog taskId = {task._id}/>
-                                    </Stack>
-                                    <Box
-                                        component='div'
-                                        sx={{
-                                            backgroundColor: 'primary.main',
-                                            borderRadius: '100px',
-                                            p: '4px 16px',
-                                            lineHeight: '0px',
-                                            color: 'primary.contrastText',
-                                            fontWeight: '600',
-                                            fontSize: '18px'
-                                        }}>
-                                        <Typography variant='caption' align='center'>{task.status}</Typography>
-                                    </Box>
+                            <Typography variant='h5' component='h2' sx={{ mb: '0.75rem', color: 'grey.800' }}>{task.todo}</Typography>
+                            <Typography variant='subtitle1' sx={{ mb: '0.75rem', color: 'grey.700' }}>{task.description}</Typography>
+                            <Stack direction='raw' justifyContent='space-between' alignItems='center'>
+                                <Stack direction='raw' alignItems='center'>
+                                    <Typography>{new Date(task.createdAt).toLocaleString('en-us')}</Typography>
+                                    <EditModal taskId={task._id} status={task.status} />
+                                    <DeleteDialog taskId={task._id} />
                                 </Stack>
+
+                                <Typography
+                                    variant='overline'
+                                    component='div'
+                                    align='center'
+                                    sx={{
+                                        bgcolor: 'primary.main',
+                                        p: '2px 12px',
+                                        color: 'white',
+                                        borderRadius: '1000px',
+                                        fontSize: '10px',
+                                        fontWeight: 500,
+                                        letterSpacing: '1px'
+                                    }}
+                                >
+                                    {task.status}
+                                </Typography>
+                            </Stack>
                         </Paper>
                     </Grid>
                 ))
